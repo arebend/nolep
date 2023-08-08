4afb62530c829c01dafb6989a25512788c00e335
AronIpeka <aron.benedictus@ho.ipeka.sch.id>
2023-08-08 15:52:42 +0700

    add

---
+++ Part 10 - Rekursi/soal_1.js
@@ -0,0 +1,52 @@
+/*
+Implementasikan function sorting dan getTotal untuk mendapatkan angka yang paling besar dan mengetahui berapa kali angka tersebut muncul di dalam arrNumber.
+
+Dengan HANYA mengubah code di dalam 2 function yang diberikan (sorting dan getTotal). Dilarang mengubah isi dalam function mostFrequentLargestNumbers!
+*/
+function sorting(arrNumber) {
+  // code di sini
+  return arrNumber.sort();
+}
+
+function getTotal(arrNumber) {
+  // code di sini
+  var counter = 1; //inisialisasi counter
+  var biggestNum = arrNumber[0]; //inisialisasi biggestNum
+
+  if (arrNumber.length == 0) {
+    //kalau array kosong
+    return "";
+  }
+  for (let i = 1; i < arrNumber.length; i++) {
+    //periksa satu per satu isi arrray
+    if (arrNumber[i] > biggestNum) {
+      //bila data array lebih besar dari biggestNum saat ini
+      biggestNum = arrNumber[i]; //override
+      counter = 1; //reset counter
+    } else if (arrNumber[i] == biggestNum) {
+      //bila data array adalah angka yang sama
+      counter++; //counter bertambah 1
+    }
+  }
+  return `angka paling besar adalah ${biggestNum} dengan jumlah kemunculan sebanyak ${counter} kali`;
+}
+
+function mostFrequentLargestNumbers(arrNumber) {
+  var listSort = sorting(arrNumber);
+  var countHighest = getTotal(listSort);
+  return countHighest;
+}
+
+console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+//'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+
+console.log(
+  mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150])
+);
+//'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+
+console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+//'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+
+console.log(mostFrequentLargestNumbers([]));
+//''
---
+++ Part 10 - Rekursi/soal_2.js
@@ -0,0 +1,68 @@
+/*
+Diberikan function changeVocals, reverseWord, setLowerUpperCase, removeSpaces, dan passwordGenerator
+
+Pada function passwordGenerator implementasikan requirement dibawah ini untuk membuat password (harus berurutan):
+
+Ganti semua huruf vokal menggunakan function changeVocals dengan aturan huruf vokal yang diganti akan menjadi huruf setelah huruf vokal itu (ex: a -> b, i -> j, u -> v, e -> f, o -> p, A -> B, I -> J, U -> V, E -> F, O -> P)
+
+Balikkan/reverse kata yang sudah kita ganti huruf vokalnya menggunakan reverseWord
+
+Gunakan function setLowerUpperCase untuk mengganti huruf besar menjadi kecil dan sebaliknya
+
+Gunakan function removeSpaces untuk menghilangkan semua spasi di dalam string yang sudah kita manipulasi
+*/
+function changeVocals(str) {
+  //code di sini
+  const vokal = ["a", "i", "u", "e", "o", "A", "I", "U", "E", "O"];
+  const arr = str.split("");
+  for (let i = 0; i < vokal.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === vokal[i]) {
+        arr[j] = String.fromCharCode(arr[j].charCodeAt() + 1);
+      }
+    }
+  }
+  return reverseWord(arr.join(""));
+}
+
+function reverseWord(str) {
+  //code di sini
+  const arr = str.split("");
+  const reverse = [];
+  for (let i = arr.length + 1; i >= 0; i--) {
+    reverse.push(arr[i]);
+  }
+  return setLowerUpperCase(reverse.join(""));
+}
+
+function setLowerUpperCase(str) {
+  //code di sini
+  const arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i].toUpperCase()) {
+      arr[i] = arr[i].toUpperCase();
+    } else if (arr[i] !== arr[i].toLowerCase()) {
+      arr[i] = arr[i].toLowerCase();
+    }
+  }
+  return removeSpaces(arr.join(""));
+}
+
+function removeSpaces(str) {
+  //code di sini
+  const arr = str.split(" ");
+  return arr.join("");
+}
+
+function passwordGenerator(name) {
+  //code di sini
+  if (name.length < 5) {
+    return "Minimal karakter yang diinputkan adalah 5 karakter";
+  }
+  return changeVocals(name);
+}
+
+console.log(passwordGenerator("Sergei Dragunov")); // 'VPNVGBRdJFGRFs'
+console.log(passwordGenerator("Dimitri Wahyudiputra")); // 'BRTVPJDVYHBwJRTJMJd'
+console.log(passwordGenerator("Alexei")); // 'JFXFLb'
+console.log(passwordGenerator("Alex")); // 'Minimal karakter yang diinputkan adalah 5 karakter'

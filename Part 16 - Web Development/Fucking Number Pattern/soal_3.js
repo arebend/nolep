b9142536c3eae274b1466a7ca03162e2ed8705a8
AronIpeka <aron.benedictus@ho.ipeka.sch.id>
2024-02-27 08:58:23 +0700

    oke

--- Part 16 - Web Development/Fucking Number Pattern/soal_3.js
+++ Part 16 - Web Development/Fucking Number Pattern/soal_3.js
@@ -67,17 +67,17 @@ function squareNumber(num) {
 console.log(squareNumber(3));
 // [ [ 7, 8, 9 ], [ 6, 5, 4 ], [ 1, 2, 3 ] ]
 
-// console.log(squareNumber(4));
-// // [ [ 16, 15, 14, 13 ],
-// //   [ 9, 10, 11, 12 ],
-// //   [ 8, 7, 6, 5 ],
-// //   [ 1, 2, 3, 4 ] ]
+console.log(squareNumber(4));
+// [ [ 16, 15, 14, 13 ],
+//   [ 9, 10, 11, 12 ],
+//   [ 8, 7, 6, 5 ],
+//   [ 1, 2, 3, 4 ] ]
 
-// console.log(squareNumber(5));
-// // [ [ 21, 22, 23, 24, 25 ],
-// //   [ 20, 19, 18, 17, 16 ],
-// //   [ 11, 12, 13, 14, 15 ],
-// //   [ 10, 9, 8, 7, 6 ],
-// //   [ 1, 2, 3, 4, 5 ] ]
+console.log(squareNumber(5));
+// [ [ 21, 22, 23, 24, 25 ],
+//   [ 20, 19, 18, 17, 16 ],
+//   [ 11, 12, 13, 14, 15 ],
+//   [ 10, 9, 8, 7, 6 ],
+//   [ 1, 2, 3, 4, 5 ] ]
 
-// console.log(squareNumber(2)); // Minimal input adalah 3
+console.log(squareNumber(2)); // Minimal input adalah 3
